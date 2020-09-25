245bcd7c4cca92e15f61cfbe382902373d91e17b
kohei-kohei <sky6sk212@gmail.com>
2020-09-25 14:10:52 +0900

    filter ads

--- dsp/req/dsp.php
+++ dsp/req/dsp.php
@@ -1,6 +1,28 @@
 <?php
+// bidRequestを受け取る
 $res = file_get_contents('php://input');
 $bidResponse = json_decode($res, true);
-echo $bidResponse['bidfloor']."\n";
 
-echo "HTTP/1.1 200 OK";
+// responseを返す
+echo "HTTP/1.1 200 OK"."\n";
+
+// 広告リストの取得
+$res = file_get_contents('/Users/sakuta/web/fancomi_intern/ready-for-miniDSP-Internship/ads.json');
+$ads = json_decode($res, true);
+
+// 広告のフィルタリング
+$filtered_ads = array();
+for($i = 0; $i < count($ads); ++$i) {
+    $counts = 0;
+    $ids = $ads[$i]['blocked_app_ids'];
+    for($j = 0; $j < count($ids); ++$j) {
+        if ($bidResponse['app_id'] === $ids[$j]) {
+            break;
+        } else {
+            $counts++;
+        }
+    }
+    if ($counts === count($ids)) {
+        array_push($filtered_ads,  $ads[$i]);
+    }
+}
